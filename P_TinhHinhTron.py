38bdf7eae795e2e7182052d0763adb462b0f877e
TamNhien <canhthien91@gmail.com>
2024-03-18 21:24:28 +0700

    Add project files.

---
+++ P_TinhHinhTron.py
@@ -0,0 +1,14 @@
+﻿import math
+
+def main():
+    r = float(input("Nhập bán kính của hình tròn : "))
+
+    P = 2 * math.pi * r
+    S = math.pi * r ** 2
+
+    print(f"Chu vi của hình tròn là : {P}")
+    print(f"Diện tích của hình tròn là : {S}")
+
+if __name__ == "__main__":
+    main()
+
